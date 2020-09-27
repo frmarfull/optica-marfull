e628bb574d7982ac5219f86a1eef591dbfc1c84f
frmarfull <fr.marfull@alumnos.duoc.cl>
2020-09-27 03:20:56 -0300

    Consultar y solicitar al 70%, cambios en el script, rut, index

--- asset/js/script.js
+++ asset/js/script.js
@@ -16,12 +16,122 @@
     var instances = M.Modal.init(elems);
   });
 
+var hidden = false;
+
+//Función para ocultar o mostrar determinadas secciones.
+function toggleAll(){
+	//Se buscan las secciones que serán ocultadas.
+	var header = document.getElementById("headerprincipal");
+	var desc = document.getElementById("descuentos");
+	var cli = document.getElementById("clientes");
+	
+	if (hidden){
+	  //Mostrar la imagen principal.
+	  header.style.minHeight = "700px";
+	  //Mostrar descuentos.
+	  desc.style.display = "block";
+	  //Mostrar clientes.
+	  cli.style.display = "block";
+	  
+	  //ocultar otros elementos recurrentes.
+	  ocultarPorId("catalogo")
+	  ocultarPorId("consultar")
+	  ocultarPorId("solicitar")
+	  //ocultarPorId("modificar")
+	  //ocultarPorId("cancelar")
+	  
+	  
+	  hidden = false;
+	} 
+	else {
+	  //Ocultar la imagen principal.
+	  header.style.minHeight = "0";
+	  //Ocultar descuentos.
+	  desc.style.display = "none";
+	  //Ocultar clientes.
+	  cli.style.display = "none";
+	  
+	  //ocultar otros elementos recurrentes.
+	  ocultarPorId("catalogo")
+	  ocultarPorId("consultar")
+	  ocultarPorId("solicitar")
+	  //ocultarPorId("modificar")
+	  //ocultarPorId("cancelar")
+	  
+	  hidden = true;
+	}		
+}
+
+function ocultar(){
+	hidden = false;
+	toggleAll();
+}
+
+function mostrar(){
+	hidden = true;
+	toggleAll();
+}
+
+function verPorId(id){
+	var elemento = document.getElementById(id);
+	if (elemento==null) {return "no existe!";}
+	//Ocultar los elementos distractorios.
+	ocultar()
+	
+	elemento.style.display = "block";
+}
+
+function ocultarPorId(id){
+	var elemento = document.getElementById(id);
+	elemento.style.display = "none";
+}
+
+function verCatalogo(){
+	verPorId("catalogo");
+}
+
+function ver(elemento){
+	if (verPorId(elemento)=="no existe!"){
+		alert("¡Error de referencia!.");
+	}
+}
+
+function getDespachoArray() {
+  return JSON.parse(localStorage.getItem("Despacho"));
+}
+
+function createTablaRut(listado) {
+  for (i = 0; i < listado.length; i++) {
+  var tablarut = document.getElementById("tablarut");
+  var fila = document.createElement("tr");
+  var celda1 = document.createElement("td");
+  var celda2 = document.createElement("td");
+  var celda3 = document.createElement("td");
+  console.log("Elementos creados.")
+  celda1.innerHTML = i;
+  celda2.innerHTML = listado[i].nombre+" "+listado[i].apellido;
+  celda3.innerHTML = listado[i].calle+", "+listado[i].comuna;
+  fila.appendChild(celda1);
+  fila.appendChild(celda2);
+  fila.appendChild(celda3);
+  tablarut.appendChild(fila);
+  }
+}
+
 formrut.addEventListener('submit', (e)=>{
     e.preventDefault();
-    let tablaUI=document.querySelector('#rut').value.toLowerCase();
+    //let valuerut=document.querySelector('#rut').value.toLowerCase();
+    //console.log(valuerut);
+    
+    //Ejemplo de cómo quedará implementada la tabla con los datos del cliente
     document.getElementById("tablarut").className = "striped";
     
-    })
+    var tablarut = document.getElementById("tablarut");
+    
+    listaDespacho = getDespachoArray();
+    createTablaRut(listaDespacho);
+    
+})
 // INTENTO EXITOSO DE MAPA USANDO LA API DE GOOGLE MAPS
 /*
   function initialize() {
--- asset/js/validar-rut.js
+++ asset/js/validar-rut.js
@@ -1,36 +1,36 @@
-function checkRut(rut) {
+ï»¿function checkRut(rut) {
     // Despejar Puntos
     var valor = rut.value.replace('.','');
-    // Despejar Guión
+    // Despejar GuiÃ³n
     valor = valor.replace('-','');
     
-    // Aislar Cuerpo y Dígito Verificador
+    // Aislar Cuerpo y DÃ­gito Verificador
     cuerpo = valor.slice(0,-1);
     dv = valor.slice(-1).toUpperCase();
     
     // Formatear RUN
     rut.value = cuerpo + '-'+ dv
     
-    // Si no cumple con el mínimo ej. (n.nnn.nnn)
+    // Si no cumple con el mÃ­nimo ej. (n.nnn.nnn)
     if(cuerpo.length < 7) { 
 		rut.setCustomValidity("RUT Incompleto");
 		return false;
 		}
     
-    // Calcular Dígito Verificador
+    // Calcular DÃ­gito Verificador
     suma = 0;
     multiplo = 2;
     
-    // Para cada dígito del Cuerpo
+    // Para cada dÃ­gito del Cuerpo
     for(i=1;i<=cuerpo.length;i++) {
     
-        // Obtener su Producto con el Múltiplo Correspondiente
+        // Obtener su Producto con el MÃºltiplo Correspondiente
         index = multiplo * valor.charAt(cuerpo.length - i);
         
         // Sumar al Contador General
         suma = suma + index;
         
-        // Consolidar Múltiplo dentro del rango [2,7]
+        // Consolidar MÃºltiplo dentro del rango [2,7]
         if(multiplo < 7) {
 			multiplo = multiplo + 1;
 		}
@@ -40,19 +40,19 @@ function checkRut(rut) {
   
     }
     
-    // Calcular Dígito Verificador en base al Módulo 11
+    // Calcular DÃ­gito Verificador en base al MÃ³dulo 11
     dvEsperado = 11 - (suma % 11);
     
     // Casos Especiales (0 y K)
     dv = (dv == 'K')?10:dv;
     dv = (dv == 0)?11:dv;
     
-    // Validar que el Cuerpo coincide con su Dígito Verificador
+    // Validar que el Cuerpo coincide con su DÃ­gito Verificador
     if(dvEsperado != dv) {
-		rut.setCustomValidity("RUT inválido"); 
+		rut.setCustomValidity("RUT invÃ¡lido"); 
 		return false;
 		}
     
-    // Si todo sale bien, eliminar errores (decretar que es válido)
+    // Si todo sale bien, eliminar errores (decretar que es vÃ¡lido)
     rut.setCustomValidity('');
 }
