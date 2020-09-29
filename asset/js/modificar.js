2f097272c445379ca0155cdde3445aa1fec243ed
frmarfull <fr.marfull@alumnos.duoc.cl>
2020-09-28 21:06:57 -0300

    Modificar completado

--- asset/js/modificar.js
+++ asset/js/modificar.js
@@ -1,3 +1,6 @@
+let campoRut
+let datosUsuario
+
 // Eventos
 formModificarRut.addEventListener('submit', (e)=>{
     e.preventDefault();
@@ -5,15 +8,31 @@ formModificarRut.addEventListener('submit', (e)=>{
     campoRut = document.getElementById("rutInputModificar").value;
 
     // Se buscan coincidencias del rut.
-    var encontrado = encuentraRutModificar(campoRut);
-    if (encontrado != false){
+    datosUsuario = encuentraRutModificar(campoRut);
+    if (datosUsuario != false){
         //Si se encuentra el rut, se procede a mostrar los campos disponibles.
         var formDatosModificarUI = document.getElementById("formDatosModificar");
-        //Sacamos la clase ocultar, para hacer visible el formulario.
+        //Remover la clase "ocultar", para hacer visible el formulario.
         formDatosModificarUI.className = "";
-        
-        // Rellenar los campos.
-        
+
+        // Rellenar los campos a modificar.
+        nombreMod = datosUsuario["nombre"]
+        formDatosModificarUI[0].value = nombreMod;
+    
+        apellidoMod = datosUsuario["apellido"]
+        formDatosModificarUI[1].value = apellidoMod;
+    
+        regionMod = datosUsuario["region"]
+        formDatosModificarUI[2].value = regionMod;
+    
+        calleMod = datosUsuario["calle"]
+        formDatosModificarUI[4].value = calleMod;
+    
+        comunaMod = datosUsuario["comuna"]
+        formDatosModificarUI[3].value = comunaMod;
+
+        depaMod = datosUsuario["departamento"]
+        formDatosModificarUI[5].value = depaMod;
     }
     else {
         alert("No se enuentran coincidencias");
@@ -32,70 +51,31 @@ function encuentraRutModificar(campoRut){
     return encontrado;
 }
 
-/*
-var datoTemp;
-//funciones.
-function encontrarAndRellenar(){
-    var tablaModificarRut = document.getElementById("tablaModificarRut");
-    var formDatosModificarUI = document.getElementById("formDatosModificar");
-    
-    datoTemp = buscarCoincidencia('#formModificarRut #rut');
-
-    nombreMod = tablaModificarRut.rows[1].cells[3].innerText
-    formDatosModificarUI[0].value = nombreMod;
-
-    apellidoMod = tablaModificarRut.rows[1].cells[4].innerText
-    formDatosModificarUI[1].value = apellidoMod;
-
-    regionMod = tablaModificarRut.rows[1].cells[5].innerText
-    formDatosModificarUI[2].value = regionMod;
-
-    calleMod = tablaModificarRut.rows[1].cells[6].innerText
-    formDatosModificarUI[4].value = calleMod;
-
-    comunaMod = tablaModificarRut.rows[1].cells[7].innerText
-    formDatosModificarUI[3].value = comunaMod;
-
-    depaMod = tablaModificarRut.rows[1].cells[8].innerText
-    formDatosModificarUI[5].value = depaMod;
-}
-
-// Eventos
-formModificarRut.addEventListener('submit', (e)=>{
-    e.preventDefault();
-    tablaModificarRut.className = "striped";
-    formDatosModificarUI.className = "";
-    limpiarTabla("tablaModificarRut");
-    encontrarAndRellenar();
-})
-
 formDatosModificar.addEventListener('submit', (e)=>{
     e.preventDefault();
+    var formDatosModificarUI = document.getElementById("formDatosModificar");
+    
+    rut = campoRut;
+    nombre = formDatosModificarUI[0].value;
+    apellido = formDatosModificarUI[1].value;
+    region = formDatosModificarUI[2].value;
+    calle = formDatosModificarUI[4].value;
+    comuna = formDatosModificarUI[3].value;
+    departamento = formDatosModificarUI[5].value;
 
-    rutUI2 = document.getElementById('formModificarRut')[0].value
-    nombreUI2 = formDatosModificarUI[0].value;
-    apellidoUI2 = formDatosModificarUI[1].value;
-    regionUI2 = formDatosModificarUI[2].value;
-    calleUI2 = formDatosModificarUI[4].value;
-    comunaUI2 = formDatosModificarUI[3].value;
-    departamentoUI2 = formDatosModificarUI[5].value;
+    cod = datosUsuario["cod"];
+    desc = datosUsuario["desc"];
+    precio = datosUsuario["precio"];
 
-    codUI0 = tablaModificarRut.rows[1].cells[1].innerText;
-    descUI0 = tablaModificarRut.rows[1].cells[2].innerText;
-    precioUI0 = tablaModificarRut.rows[1].cells[9].innerText;
-
-    //borrar el registro del array.
-    value = datoTemp;
-    arrayDatosDespacho = arrayDatosDespacho.filter(item => item !== value);
-
-    crearItem(rutUI2,nombreUI2,apellidoUI2,regionUI2,comunaUI2,calleUI2,departamentoUI2, codUI0, descUI0, precioUI0);
-    guardarDatos();
-
+    index = arrayDatosDespacho.findIndex(x => x.rut ===rut);
+    if(index !=-1){
+        arrayDatosDespacho[index]={rut,nombre,apellido,region,
+            comuna,calle,departamento,cod,desc,precio}
+            guardarDatos();
+        }
     alert("Datos modificados!");
     formModificarRut.reset();
     formDatosModificar.reset();
-    limpiarTabla("tablaModificarRut");
-    tablaModificarRut.className = "hide";
     formDatosModificar.className = "hide";
+    
 })
-*/
